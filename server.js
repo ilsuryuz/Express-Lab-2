9c24f070ade5d0cf3667caeecb9962d4d6c2f0af
Ilsur <yuzlikeyevi@gmail.com>
2022-08-30 20:56:38 -0500

    Greeting express application created

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const port = 3000;
 
 
 app.get("/greeting/", (req, res)=> {
-    res.send("Hello, stranger")
-})
+    res.send("Hello, stranger");
+});
 
+app.get("/greeting/:name", (req, res) => {
+    res.send("Howdy " + req.params.name + "!")
+})
 
 app.listen(port, () =>{
-    console.log("Chicago is listening")
-})
+    console.log("Chicago is listening");
+});
